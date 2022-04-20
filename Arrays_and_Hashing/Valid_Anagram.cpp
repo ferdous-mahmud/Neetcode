9f0fe22b3970dcf6c17c95555b1f5589fc9e0e05
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-20 23:16:57 +0600

    Valid Anagram O(nlogn) added

---
+++ Arrays_and_Hashing/Valid_Anagram.cpp
@@ -0,0 +1,19 @@
+// Valid Anagram Leetcode problem
+
+
+
+// Solution usng sort()
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        if( s == t){
+            return true;
+        }
+        return false;
+    }
+};
