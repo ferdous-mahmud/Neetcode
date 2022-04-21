c8bb6a8f38f182b9e7b66eb94184b99a3c5279e0
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-21 11:42:16 +0600

    Valid Anagram O(n) added

--- Arrays_and_Hashing/Valid_Anagram.cpp
+++ Arrays_and_Hashing/Valid_Anagram.cpp
@@ -1,5 +1,30 @@
 // Valid Anagram Leetcode problem
+// Solution usng sort()
+// Time Complexity: O(n)
+// Space Complexity: O(n)  **using extra space
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        
+        map<char, int> sMap, tMap;
+        for(int i = 0; i < s.size(); i++){
+            sMap[s[i]] += 1;
+            tMap[t[i]] += 1;
+        }
+        
+        for(auto i: s){
+            if(sMap[i] != tMap[i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
 
 
 // Solution usng sort()
