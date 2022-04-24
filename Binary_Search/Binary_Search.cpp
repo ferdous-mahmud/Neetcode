0120d4d5b2d5404b3722afd0b11f89526c428f2b
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-24 16:58:37 +0600

    Binary Search implemented

---
+++ Binary_Search/Binary_Search.cpp
@@ -0,0 +1,35 @@
+// Binary Search Leetcode problem
+// Time Complexiyt: O(logn)
+// Space Complexity: O(1)
+// Array must be sorted
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while(right >= left){
+            
+            if(nums[left] == target){
+                return left;
+            }
+            if(nums[right] == target){
+                return right;
+            }
+            
+            int mid = (left + right) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
